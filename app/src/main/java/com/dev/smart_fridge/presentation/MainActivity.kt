0578ae70461bceb043389182c7d5acd3e06a5bef
romam <romamar2004@gmail.com>
2023-10-31 20:31:16 +0300

    Initial commit

---
+++ app/src/main/java/com/dev/smart_fridge/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.smart_fridge.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.smart_fridge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
