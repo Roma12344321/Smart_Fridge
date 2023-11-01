d7dac081d4946943d620c7da4e0c93b333d99aca
vladimir <belaevvova999@gmail.com>
2023-11-01 16:55:13 +0300

    bottom bar upd

--- app/src/main/java/com/dev/smart_fridge/presentation/MainActivity.kt
+++ app/src/main/java/com/dev/smart_fridge/presentation/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.dev.smart_fridge.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.lifecycle.ViewModelProvider
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.dev.smart_fridge.R
-import com.dev.smart_fridge.domain.Product
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        setupWithNavController(bottomNavigationView, navController)
     }
 }
