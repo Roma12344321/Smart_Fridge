e91ad6ed9d747ab98354cf03fd815a144909247c
vladimir <belaevvova999@gmail.com>
2024-01-01 21:21:29 +0300

    first model

--- app/src/main/java/com/dev/smart_fridge/presentation/MainViewModel.kt
+++ app/src/main/java/com/dev/smart_fridge/presentation/MainViewModel.kt
@@ -1,11 +1,14 @@
 package com.dev.smart_fridge.presentation
 
+
 import androidx.lifecycle.ViewModel
+import com.dev.smart_fridge.BuildConfig
 import com.dev.smart_fridge.domain.AddProductUseCase
 import com.dev.smart_fridge.domain.DeleteProductUseCase
 import com.dev.smart_fridge.domain.GetAllProductUseCase
 import com.dev.smart_fridge.domain.GetProductItemUseCase
 import com.dev.smart_fridge.domain.Product
+import com.google.ai.client.generativeai.GenerativeModel
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
@@ -14,6 +17,11 @@ class MainViewModel @Inject constructor(
     private val getAllProductUseCase: GetAllProductUseCase,
     private val getProductItemUseCase: GetProductItemUseCase,
 ) : ViewModel() {
+
+    val generativeModel = GenerativeModel(
+        modelName = "gemini-pro-vision",
+        apiKey = BuildConfig.apiKey,
+    )
 
 
     fun addProduct(product: Product) {
